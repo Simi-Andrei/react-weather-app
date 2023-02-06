32ddde8600ca8cebeb44a23a07562e3db57e6577
SimigiuAndrei <andy_andrei9572@yahoo.com>
2023-02-06 14:00:21 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="layout w-full flex flex-col min-h-screen justify-between bg-blue-900">
       <Header />
-      <main className="container mx-auto bg-blue-900 flex-1 text-blue-100 p-4 border-x border-blue-300">
+      <main className="container mx-auto bg-blue-900 flex-1 text-blue-100 lg:p-4 border-x border-blue-300">
         <Router>
           <Routes>
             <Route path="/" element={<Homepage />} />
